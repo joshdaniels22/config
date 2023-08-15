cac087fcd114b35eb83c193093a153e24c999c04
Joshua Daniels <joshuadaniels537@gmail.com>
2023-08-15 22:36:29 +0200

    feature: install jump on setup

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 
 echo 'source $(pwd)/terminal_shortcuts' >> ~/.bashrc
 source ~/.bashrc
+
+wget https://github.com/gsamokovarov/jump/releases/download/v0.51.0/jump_0.51.0_amd64.deb && sudo dpkg -i jump_0.51.0_amd64.deb
