688986e1a9b594df28ef82dd252cb1d7b257f40c
Joshua Daniels <joshua.daniels@allangray.co.za>
2023-08-24 17:59:53 +0200

    fix: change quotes to recognise string manipulation

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-echo 'source $(pwd)/terminal_shortcuts' >> ~/.bashrc
+echo "source $(pwd)/terminal_shortcuts" >> ~/.bashrc
 source ~/.bashrc
 
 # wget https://github.com/gsamokovarov/jump/releases/download/v0.51.0/jump_0.51.0_amd64.deb && sudo dpkg -i jump_0.51.0_amd64.deb
