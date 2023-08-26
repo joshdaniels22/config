71ec154be96eb630cdad5fc175070acf36c68258
Joshua Daniels <joshuadaniels537@gmail.com>
2023-08-26 11:52:29 +0200

    fix: jump install working

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
-#!/bin/bash 
+#!/bin/bash
 
+# Put Link to Shortcuts in .bashrc File
 echo "source $(pwd)/terminal_shortcuts" >> ~/.bashrc
-source ~/.bashrc
 
-# wget https://github.com/gsamokovarov/jump/releases/download/v0.51.0/jump_0.51.0_amd64.deb && sudo dpkg -i jump_0.51.0_amd64.deb
+# Install jump Command
+wget https://github.com/gsamokovarov/jump/releases/download/v0.51.0/jump_0.51.0_amd64.deb && sudo dpkg -i jump_0.51.0_amd64.deb
+
+# Refresh .bashrc so that it works in the current shell session
+# doesn't work
+# source ~/.bashrc
+
+echo "Done. Open a new shell to use commands."
